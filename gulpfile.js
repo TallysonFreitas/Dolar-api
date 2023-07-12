b5578e193c46b267f98451f3505e3062a333ec7a
Tallyson Freitas <tallyson.eu@outlook.com>
2023-07-12 14:05:30 -0300

    build

--- gulpfile.js
+++ gulpfile.js
@@ -13,3 +13,6 @@ function compilaSass() {
 exports.default = function () {  
     gulp.watch('./src/styles/*.scss', {ignoreInitial:false}, gulp.parallel(compilaSass))
 }
+exports.build = function () {  
+    gulp.parallel(compilaSass)
+}
